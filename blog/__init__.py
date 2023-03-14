62cf7911ab2b1ccf043213a10907abd39f2d1481
Esmaeil kazemi <smk182018@gmail.com>
2023-03-14 10:10:39 +0330

    fix(): fix bug and PEP8

--- blog/__init__.py
+++ blog/__init__.py
@@ -2,8 +2,8 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///../blog.db"
 
-db = SQLAlchemy[app]
+db = SQLAlchemy(app)
 
 from blog import routes
