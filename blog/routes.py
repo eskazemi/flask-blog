b1cc1ad4ab869f27210da45442022c6b061ef67e
Esmaeil kazemi <smk182018@gmail.com>
2023-03-15 11:41:10 +0330

    Feat(): create routers related to forms

--- blog/routes.py
+++ blog/routes.py
@@ -1,7 +1,20 @@
 from blog import app
 from flask import render_template
+from blog.forms import RegistrationForm
 
 
-@app.route("/")
-def get_name():
+@app.route("/", methods=["GET"])
+def home():
     return render_template('home.html')
+
+
+@app.route("/register")
+def register():
+    form = RegistrationForm()
+    return render_template("register.html", form=form)
+
+
+@app.route("/login")
+def login():
+    form = RegistrationForm()
+    return render_template("login.html", form=form)
