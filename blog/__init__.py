f4f74b2af7501f82f93e1e8468a07a5c45a39052
Esmaeil kazemi <smk182018@gmail.com>
2023-03-15 11:39:03 +0330

    Feat(): add secret key to config

--- blog/__init__.py
+++ blog/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = '20087e54287ebc42171363753890098b'
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///../blog.db"
 
 db = SQLAlchemy(app)
