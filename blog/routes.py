356ce24055fb3001f9d82d53592d24614f4a6dac
eskazemi <smk182018@gmail.com>
2023-03-20 00:58:46 +0330

    feat():Implementation of user registration function

--- blog/routes.py
+++ blog/routes.py
@@ -1,6 +1,22 @@
-from blog import app
-from flask import render_template
-from blog.forms import RegistrationForm
+from blog import (
+    app,
+    db,
+    bcrypt,
+)
+from flask import (
+    render_template,
+    redirect,
+    url_for,
+    request,
+)
+from blog.forms import (
+    RegistrationForm,
+    LoginForm,
+)
+from blog.models import (
+    User,
+    Post,
+)
 
 
 @app.route("/", methods=["GET"])
@@ -8,10 +24,19 @@ def home():
     return render_template('home.html')
 
 
-@app.route("/register")
+@app.route("/register", methods=["GET", "POST"])
 def register():
     form = RegistrationForm()
-    return render_template("register.html", form=form)
+    if request.method == "GET":
+        return render_template("register.html", form=form)
+    else:
+        if form.validate_on_submit():
+            p_h = bcrypt.generate_password_hash(form.password.data).decode("utf-8")
+            user = User(username=form.username.data, email=form.email.data,
+                        password=p_h)
+            db.session.add(user)
+            db.session.commit()
+        return redirect(url_for("home"))
 
 
 @app.route("/login")
