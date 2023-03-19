2a05fe3414af8d6743b1704f289ff8ccf0131bc4
eskazemi <smk182018@gmail.com>
2023-03-20 00:54:21 +0330

    feat(): install flask_bcrypt package

--- blog/__init__.py
+++ blog/__init__.py
@@ -1,9 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = '20087e54287ebc42171363753890098b'
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///../blog.db"
+
+bcrypt = Bcrypt(app)
 
 db = SQLAlchemy(app)
 
