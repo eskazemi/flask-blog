2b2b5c60da56bf4056fc814670d12f3d51e30dc1
eskazemi <smk182018@gmail.com>
2023-03-20 15:08:50 +0330

    fix(): update register user

--- blog/routes.py
+++ blog/routes.py
@@ -7,7 +7,7 @@ from flask import (
     render_template,
     redirect,
     url_for,
-    request,
+    flash,
 )
 from blog.forms import (
     RegistrationForm,
@@ -27,19 +27,23 @@ def home():
 @app.route("/register", methods=["GET", "POST"])
 def register():
     form = RegistrationForm()
-    if request.method == "GET":
-        return render_template("register.html", form=form)
-    else:
-        if form.validate_on_submit():
-            p_h = bcrypt.generate_password_hash(form.password.data).decode("utf-8")
-            user = User(username=form.username.data, email=form.email.data,
-                        password=p_h)
-            db.session.add(user)
-            db.session.commit()
+    if form.validate_on_submit():
+        p_h = bcrypt.generate_password_hash(form.password.data).decode("utf-8")
+        user = User(username=form.username.data, email=form.email.data,
+                    password=p_h)
+        db.session.add(user)
+        db.session.commit()
+        flash("you registered successfully", 'success')
         return redirect(url_for("home"))
+    return render_template("register.html", form=form)
 
 
 @app.route("/login")
 def login():
-    form = RegistrationForm()
+    form = LoginForm()
     return render_template("login.html", form=form)
+
+
+@app.route("/logout")
+def logout():
+    return render_template("home.html")
