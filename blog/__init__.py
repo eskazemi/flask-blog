0f172b13526ec08e9da91f6dbd09939bfc925239
eskazemi <smk182018@gmail.com>
2023-03-21 21:32:03 +0330

    feat(): install flask_login and use from flask_login

--- blog/__init__.py
+++ blog/__init__.py
@@ -1,12 +1,18 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
+
 app.config["SECRET_KEY"] = '20087e54287ebc42171363753890098b'
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///../blog.db"
 
 bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message = "Please Login First"
+login_manager.login_message_category = "info"
 
 db = SQLAlchemy(app)
 
